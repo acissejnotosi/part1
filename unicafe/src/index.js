81b90b208234ba68de11573bde3ecb8b31258f07
Jessica Isoton <jessicasampaio195@gmail.com>
2020-04-14 22:40:11 +0200

    1.6: unicafe step1

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const buttonStyle = {
+  borderRadius: '4px'
+};
+
+const Title = (props) => {
+  return <h1> {props.title} </h1>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button style={buttonStyle} onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
+
+const FeedbackResult = ({ text, result }) => {
+  return (
+    <div>
+      {text} {result}
+    </div>
+  );
+};
+
+const App = () => {
+  const giveFeedbackText = "give feedback";
+  const statisticsText = "statistics";
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodCLick = () => {
+    setGood(good + 1);
+  };
+
+  const handleBadCLick = () => {
+    setBad(bad + 1);
+  };
+
+  const handleNeutralCLick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  return (
+    <div>
+      <Title title={giveFeedbackText} />
+      <Button handleClick={handleGoodCLick} text="good" />
+      <Button handleClick={handleNeutralCLick} text="neutral" />
+      <Button handleClick={handleBadCLick} text="bad" />
+      <Title title={statisticsText} />
+      <FeedbackResult text="good " result={good} />
+      <FeedbackResult text="neutral " result={neutral} />
+      <FeedbackResult text="bad " result={bad} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
